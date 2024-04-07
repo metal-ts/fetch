365cc728156681543cf7339982576dd579a9105b
Danpa_cho <danpa725@cau.ac.kr>
2024-04-07 14:33:39 +0900

    feat: add utility types

---
+++ packages/fetch/src/utils/types/index.ts
@@ -0,0 +1,49 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
+export type IncludeString = string & {}
+
+export type JsonSupported = string | number | boolean | null
+export type JsonType =
+    | Record<string, JsonSupported>
+    | Array<JsonSupported>
+    | string
+    | number
+    | boolean
+    | null
+
+export type ConcreteBoolean = true | false
+
+export type Prettify<T> = {
+    [K in keyof T]: T[K]
+} & {}
+
+export type OmitUnknown<T> = Prettify<{
+    [K in keyof T as T[K] extends infer U
+        ? unknown extends U
+            ? never
+            : K
+        : never]: T[K]
+}>
+
+export type ToRequiredField<
+    TargetRecord,
+    Keys extends keyof TargetRecord,
+> = Prettify<Omit<TargetRecord, Keys> & Required<Pick<TargetRecord, Keys>>>
+
+export type ToOptionalField<
+    TargetRecord,
+    Keys extends keyof TargetRecord,
+> = Prettify<Omit<TargetRecord, Keys> & Partial<Pick<TargetRecord, Keys>>>
+
+type IfUnknown<T, Y, N> = [unknown] extends [T] ? Y : N
+export type OptionalAndNeverAtUnknown<RecordT> = Prettify<
+    {
+        [Key in keyof RecordT as IfUnknown<
+            RecordT[Key],
+            never,
+            Key
+        >]: RecordT[Key]
+    } & Partial<{
+        [Key in keyof RecordT as IfUnknown<RecordT[Key], Key, never>]: never
+    }>
+>
