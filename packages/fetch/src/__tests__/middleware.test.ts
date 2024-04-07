f21c4695032e72d2aa8aa43a739f0bdbacde47d9
Danpa_cho <danpa725@cau.ac.kr>
2024-04-07 14:45:21 +0900

    :white_check_mark: test: add Middleware test unit

---
+++ packages/fetch/src/__tests__/middleware.test.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-console */
+import { describe, expect, it } from 'vitest'
+import { Middleware } from '../utils/middleware'
+import { label } from './utils/test.label'
+
+describe(label.unit('middleware'), () => {
+    it(label.case('should add middleware'), () => {
+        const middleware = new Middleware<{ counter: number }, { b: string }>()
+
+        let counter = 0
+        middleware.use([
+            ({ next }) => {
+                counter++
+
+                next({
+                    req: { counter },
+                })
+            },
+            ({ next }) => {
+                counter++
+
+                next({
+                    req: { counter },
+                    res: {
+                        b: 'love',
+                    },
+                })
+            },
+            ({ next }) => {
+                counter++
+
+                next({
+                    req: { counter },
+                })
+            },
+        ])
+
+        middleware.execute({ counter }, { b: '2' })
+
+        expect(counter).toBe(3)
+    })
+})
