02bd0cc2d4e4917d68994a6e294444173ecc4cbd
Danpa_cho <danpa725@cau.ac.kr>
2024-04-07 14:45:04 +0900

    :white_check_mark: test: add Procedure unit test

---
+++ packages/fetch/src/__tests__/procedure.test.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+import { describe, expect, it } from 'vitest'
+import { Procedure, ProcedureSet } from '../utils/procedure'
+import { label } from './utils/test.label'
+
+describe(label.unit('Procedure'), () => {
+    const procedure = new ProcedureSet<Array<number>>()
+    let counter = 0
+
+    it(label.case('should register procedure unit'), () => {
+        const summation: Procedure<Array<number>> = (res) => {
+            counter += res.reduce((acc, curr) => acc + curr, 0)
+            // console.log("EXECUTION", res)
+        }
+        const logger: Procedure<Array<number>> = () => {
+            // console.log("COUNTER:", counter)
+        }
+        procedure.use(summation, true)
+        procedure.use(logger)
+    })
+
+    it(label.case('should execute procedures'), () => {
+        procedure.executor([1, 2, 3])
+        expect(counter).toBe(6)
+        procedure.executor([1, 2, 3])
+        expect(counter).toBe(6)
+        procedure.executor([1, 2, 3])
+        expect(counter).toBe(6)
+    })
+})
