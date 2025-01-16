6dc8a430ef911274ecef5e50e29234bc30b29265
Danpa_cho <danpa725@cau.ac.kr>
2025-01-16 23:32:51 +0900

    ✨ feat: export updated modules

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -1,2 +1,2 @@
-export { unit, type FetchUnitShape, type InferFetchUnit } from './core/fetcher'
+export { unit, type FetchUnitShape, FetchBuilder } from './core/fetcher'
 export { router, type GetRouterConfig } from './core/router'
