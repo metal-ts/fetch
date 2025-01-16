dd896552d9b6d54001618e9e25a6d8f56ed570e4
Danpa_cho <danpa725@cau.ac.kr>
2025-01-16 23:33:25 +0900

    🧪 test: refactor api mocks into updated code-api structure

--- packages/fetch/src/__tests__/__mocks__/client.ts
+++ packages/fetch/src/__tests__/__mocks__/client.ts
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import { MetalSchemaShape, t } from '@metal-box/type'
 import * as f from '../../index'
 import { BASE_URL } from './constant'
@@ -14,55 +13,50 @@ export const api = f.router(BASE_URL, {
         login: {
             GET: f
                 .unit()
-                .set_json()
-                .set_referrer('about:client')
+                .def_json()
+                .def_default_referrer('about:client')
                 .def_response(({ json }) =>
                     ApiResponse(t.union(t.string, t.undefined)).parse(json)
                 ),
         },
     },
-
     books: {
         GET: f
             .unit()
-            .set_json()
-            .set_referrer('about:client')
+            .def_json()
+            .def_default_referrer('about:client')
             .def_response(({ json }) =>
                 ApiResponse(Model.bookList).parse(json)
             ),
-
         POST: f
             .unit()
-            .set_json()
-            .set_referrer('about:client')
+            .def_json()
+            .def_default_referrer('about:client')
             .def_body(Model.bookRequest.parse)
             .def_response(({ json }) => {
                 const parsed = ApiResponse(Model.book).parse(json)
                 return parsed
             }),
-
         $id: {
             GET: f
                 .unit()
-                .set_json()
-                .set_referrer('about:client')
-                .def_response(({ json }) =>
-                    ApiResponse(Model.book).parse(json)
-                ),
-
+                .def_json()
+                .def_default_referrer('about:client')
+                .def_response(async ({ json }) => {
+                    return ApiResponse(Model.book).parse(json)
+                }),
             PUT: f
                 .unit()
-                .set_json()
-                .set_referrer('about:client')
+                .def_json()
+                .def_default_referrer('about:client')
                 .def_body(Model.bookRequest.parse)
                 .def_response(({ json }) =>
                     ApiResponse(Model.book).parse(json)
                 ),
-
             DELETE: f
                 .unit()
-                .set_json()
-                .set_referrer('about:client')
+                .def_json()
+                .def_default_referrer('about:client')
                 .def_response(({ json }) =>
                     ApiResponse(Model.book).parse(json)
                 ),
@@ -72,8 +66,8 @@ export const api = f.router(BASE_URL, {
         $name: {
             GET: f
                 .unit()
-                .set_json()
-                .set_referrer('about:client')
+                .def_json()
+                .def_default_referrer('about:client')
                 .def_response(({ json }) => {
                     return ApiResponse(Model.bookList).parse(json)
                 }),
