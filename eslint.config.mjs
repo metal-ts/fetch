8fae35677c95e089020c9e5c6dbe9983d8661077
Danpa_cho <danpa725@cau.ac.kr>
2025-01-16 23:21:36 +0900

    🔨 chore: update eslint config file for v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,66 +1,78 @@
+// eslint.config.js
 'use strict'
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
+import importPlugin from 'eslint-plugin-import'
+import prettierPlugin from 'eslint-plugin-prettier'
+import globals from 'globals'
 
-module.exports = {
-    // env: {
-    //     browser: true,
-    //     'shared-node-browser': true,
-    //     node: true,
-    //     es6: true,
-    // },
-    languageOptions: {
-        globals: {
-            browser: true,
-            'shared-node-browser': true,
-            node: true,
-            es6: true,
-        },
-        parser: tsParser,
-    },
-    extends: [
-        'eslint:recommended',
-        'prettier',
-        'plugin:prettier/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:import/recommended',
-    ],
-    plugins: ['prettier', 'import', '@typescript-eslint'],
-    settings: {
-        'import/resolver': {
-            node: {
-                extensions: ['.js', '.ts'],
-            },
-        },
-    },
-    rules: {
-        eqeqeq: 'error',
-        'no-var': 'error',
-        'prefer-const': 'error',
-        'no-console': 'warn',
-        'prettier/prettier': ['error', { endOfLine: 'auto' }],
-        'import/order': [
-            'error',
-            {
-                alphabetize: { order: 'asc', caseInsensitive: true },
-                groups: [
-                    'builtin',
-                    'external',
-                    'internal',
-                    'parent',
-                    'sibling',
-                    'index',
-                    'object',
-                ],
-                'newlines-between': 'never',
-                pathGroupsExcludedImportTypes: ['builtin'],
-            },
-        ],
-        'sort-imports': [
-            'error',
-            {
-                ignoreDeclarationSort: true,
-            },
+export default [
+    // Global ignore patterns
+    {
+        ignores: [
+            '**/dist/**',
+            '**/node_modules/**',
+            '**/test/**',
+            '**/*.js',
+            'site',
+            'examples/*',
         ],
     },
-    ignores: ['site', 'examples/*'],
-}
+    // Configuration for TypeScript files
+    {
+        files: ['packages/**/*.ts'],
+        languageOptions: {
+            parser: tsParser,
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        plugins: {
+            prettier: prettierPlugin,
+            import: importPlugin,
+            '@typescript-eslint': typescriptEslintPlugin,
+        },
+        rules: {
+            // Prettier and TypeScript rules
+            'prettier/prettier': ['error', { endOfLine: 'auto' }],
+            '@typescript-eslint/no-unused-vars': 'warn',
+
+            // Custom rules
+            eqeqeq: 'error',
+            'no-var': 'error',
+            'prefer-const': 'error',
+            'no-console': 'warn',
+            'import/order': [
+                'error',
+                {
+                    alphabetize: { order: 'asc', caseInsensitive: true },
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                        'index',
+                        'object',
+                    ],
+                    'newlines-between': 'never',
+                    pathGroupsExcludedImportTypes: ['builtin'],
+                },
+            ],
+            'sort-imports': [
+                'error',
+                {
+                    ignoreDeclarationSort: true,
+                },
+            ],
+        },
+        settings: {
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.ts'],
+                },
+            },
+        },
+    },
+]
