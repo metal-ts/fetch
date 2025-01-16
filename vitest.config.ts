8630c1ca3f2856564ed2b96bc89d762f9aee7c8b
Danpa_cho <danpa725@cau.ac.kr>
2025-01-16 23:22:30 +0900

    🔧 chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,11 @@
-import tsconfigPaths from "vite-tsconfig-paths"
-// eslint-disable-next-line import/no-unresolved
-import { defineConfig } from "vitest/config"
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     root: process.cwd(),
     plugins: [tsconfigPaths()],
     test: {
-        root: "packages",
+        root: 'packages',
     },
     resolve: {},
 })
